a9f5d5fb3f598f7ccc57d8ca5a710472b17770e8
Ken Wheeler <thekenwheeler@gmail.com>
2018-01-16 11:58:39 -0500

    initial

---
+++ three.js
@@ -0,0 +1,5 @@
+const THREE = require('three');
+global.THREE = THREE;
+if (!window.addEventListener) window.addEventListener = () => {};
+require('three/examples/js/renderers/Projector');
+export default THREE;
